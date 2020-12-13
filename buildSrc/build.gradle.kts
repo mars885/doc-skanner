c8d206a84633631490c212a9c86069b54b08c43c
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2020-12-13 02:03:11 +0200

    Update buildSrc KGP version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -29,5 +29,5 @@ kotlinDslPluginOptions {
 
 dependencies {
     implementation("com.android.tools.build:gradle:4.1.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
 }
