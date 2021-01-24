6865fe2c895147b1bdfcf7e2745f8e7a9dac9205
Shubham <shubham2tyagi7@gmail.com>
2021-01-24 21:28:27 +0530

    splits apk to multiple arc apks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,15 @@ android {
         }
     }
 
+    splits {
+        abi {
+            enable true
+            reset()
+            include "x86", "x86_64", "armeabi-v7a", "arm64-v8a"
+            universalApk false
+        }
+    }
+    
     buildFeatures {
         viewBinding = true
     }
